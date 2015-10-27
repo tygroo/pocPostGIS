f3f0f2895eaf4e634991637b6f02de16f12b5d5d
Brice Bonheur <bbonheur@production.apside.lan>
2015-10-27 15:33:06 +0100

    premier commit pour multiprojet de poc SNA

---
+++ PocGIS/pocGIS.domaine/src/main/java/domain/dao/SpacialRefSysDAO.java
@@ -0,0 +1,13 @@
+package domain.dao;
+
+
+import domain.model.SpacialRefSys;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by bbonheur on 26/10/2015.
+ */
+@Repository
+public interface SpacialRefSysDAO extends JpaRepository<SpacialRefSys, Integer> {
+}
